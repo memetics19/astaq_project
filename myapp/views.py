c39c5148e37a72169c7c6bd99db8d8d9e01d199f
Shreeda Bhat <50732218+memetics19@users.noreply.github.com>
2020-01-20 13:08:49 +0530

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html', {'name': 'User'})
+
+
+def add(request):
+
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+
+    return render(request, "result.html", {'result': res})
